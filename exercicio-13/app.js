e4aa034909488ef4370b5b36f36483b1b728b4cf
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2021-08-07 16:16:49 -0300

    finishes my attempt at "exercicio-13"

--- exercicio-13/app.js
+++ exercicio-13/app.js
@@ -12,7 +12,12 @@ const getCatInfo = () => {
   const name = 'Marcos'
   let age = 3
   const color = 'Cinza'
+  return {name, age, color}
 }
+
+const cat = getCatInfo()
+
+console.log(`${cat.name} é um gato ${cat.color} de ${cat.age} anos.`);
 
 /*
   02
@@ -29,7 +34,9 @@ const external = () => {
     const extraInternal = () => {
       console.log(movie.toUpperCase())
     }
+    extraInternal()
   }
+  internal()
 }
 
 external()
@@ -45,6 +52,8 @@ external()
 
 let randomNumbers = [3, 2, 1]
 
+console.log(randomNumbers.reverse());
+
 /*
   04
 
@@ -58,6 +67,9 @@ let crazyArray = [
   function getMessage () { return 'hi' },
   [ 5, 96, 53  ]
 ]
+
+crazyArray.shift()
+console.log(crazyArray);
 
 /*
   05
@@ -75,6 +87,8 @@ const dogs = [
   { name: 'Zequinha', age: 7, gender: 'Male', breed: 'Poodle' },
   { name: 'Xica', age: 6, gender: 'Female', breed: 'Chihuahua' }
 ]
+
+console.log(dogs.find(dog => dog.name === 'Zequinha'));
 
 /*
   06
@@ -108,6 +122,10 @@ const dogs = [
   </section>
 */
 
+const title = document.querySelector('h1')
+
+console.log(title);
+
 /*
   07
 
@@ -115,3 +133,7 @@ const dogs = [
     página, através da classe deles;
   - Exiba esse NodeList no console.
 */
+
+const secondaryTitles = document.querySelectorAll('.secondary-title')
+
+console.log(secondaryTitles);
